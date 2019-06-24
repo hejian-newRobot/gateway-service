85db55ef1d4cc3197eb0a88caea62bf7348550e0
cutBoy <18207307296@163.com>
2019-06-24 17:20:37 +0800

    修改过滤器及路由设置

--- src/main/java/authentication/filter/AuthorizeGatewayFilter.java
+++ src/main/java/authentication/filter/AuthorizeGatewayFilter.java
@@ -3,6 +3,7 @@ package authentication.filter;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -20,7 +21,7 @@ import reactor.core.publisher.Mono;
 /**
  * 项目名称：SimpleSpringCloudGateway
  * 包名称:authentication.filter
- * 类描述：请求头部是否有Authorization信息 没有凭证信息则直接响应请求
+ * 类描述：auth请求直接放行 ，其他请求则验证请求头部是否有Authorization信息 没有凭证信息查看请求参数是否有token 没则直接响应请求 有就直接放行
  * 创建人：hejian
  * 创建时间：2019/6/19 10:46
  * 修改人：hejian
@@ -39,6 +40,13 @@ public class AuthorizeGatewayFilter implements GlobalFilter, Ordered {
      */
     private static final String AUTHORIZATION = "Authorization";
 
+    /**
+     * access_token key
+     */
+    private static final String ACCESS_TOKEN = "access_token";
+
+    @Value("${path.oauth.prefix}")
+    private String authPathPrefix;
 
     @Override
     public int getOrder() {
@@ -51,7 +59,7 @@ public class AuthorizeGatewayFilter implements GlobalFilter, Ordered {
         logger.info("send method {} request to {}", request.getMethod(), request.getURI().toString());
         //将会跳转到OAuth2认证服务 无需提前验证 是否存在AUTHORIZATION
         //其余所有访问别的服务的请求将需要携带AUTHORIZATION请求头或者是请求参数
-        if (request.getURI().toString().contains("/oauth2")) {
+        if (request.getURI().toString().contains(authPathPrefix)) {
             return chain.filter(exchange);
         }
         HttpHeaders headers = request.getHeaders();
@@ -59,9 +67,8 @@ public class AuthorizeGatewayFilter implements GlobalFilter, Ordered {
         //从header中取token为null
         if (token == null) {
             logger.warn("Authorization token from header is empty");
-            token = request.getQueryParams().getFirst(AUTHORIZATION);
+            token = request.getQueryParams().getFirst(ACCESS_TOKEN);
         }
-
         ServerHttpResponse response = exchange.getResponse();
         if (StringUtils.isEmpty(token)) {
             String msg = "token is empty ,please check request header";
--- src/main/java/com/learn/simplegateway/SimpleGatewayApplication.java
+++ src/main/java/com/learn/simplegateway/SimpleGatewayApplication.java
@@ -2,6 +2,7 @@ package com.learn.simplegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * API网关服务
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @ EnableOAuth2Sso 启用OAuth2单点登录
  */
 @SpringBootApplication(scanBasePackages = {"conf", "authentication.*"})
+@EnableEurekaClient
 public class SimpleGatewayApplication {
 
     public static void main(String[] args) {
