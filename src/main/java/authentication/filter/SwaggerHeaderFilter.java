d843d93ef24543d12a06a4b12b3b90e1a6717b50
cutBoy <18207307296@163.com>
2019-07-18 00:40:32 +0800

    解决swagger跨域问题

--- src/main/java/authentication/filter/SwaggerHeaderFilter.java
+++ src/main/java/authentication/filter/SwaggerHeaderFilter.java
@@ -1,12 +1,7 @@
 package authentication.filter;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.web.server.ServerWebExchange;
-
-import conf.SwaggerResourceProvider;
 
 /**
  * 项目名称：SimpleSpringCloudGateway
@@ -24,18 +19,21 @@ public class SwaggerHeaderFilter extends AbstractGatewayFilterFactory {
 
     private static final String HEADER_NAME = "X-Forwarded-Prefix";
 
+    /**
+     * 废弃的过滤指令：
+     * ServerHttpRequest request = exchange.getRequest();
+     * String path = request.getURI().getPath();
+     * if (!StringUtils.endsWithIgnoreCase(path, SwaggerResourceProvider.API_URI)) {
+     * return chain.filter(exchange);
+     * }
+     * String basePath = path.substring(0, path.lastIndexOf(SwaggerResourceProvider.API_URI));
+     * ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
+     * ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
+     */
     @Override
     public GatewayFilter apply(Object config) {
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            String path = request.getURI().getPath();
-            if (!StringUtils.endsWithIgnoreCase(path, SwaggerResourceProvider.API_URI)) {
-                return chain.filter(exchange);
-            }
-            String basePath = path.substring(0, path.lastIndexOf(SwaggerResourceProvider.API_URI));
-            ServerHttpRequest newRequest = request.mutate().header(HEADER_NAME, basePath).build();
-            ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
-            return chain.filter(newExchange);
-        };
+        return (exchange, chain) -> chain.filter(exchange);
     }
+
+
 }
